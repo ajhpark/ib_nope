6610ede75f88cbfdbd4b1de5df713a910f6275b8
Andrew <andrewparkdev@gmail.com>
2021-03-06 21:00:08 -0800

    Implement exit_positions

--- nope/nope_strategy.py
+++ nope/nope_strategy.py
@@ -1,33 +1,46 @@
 import asyncio
 from datetime import datetime
 
-from ib_insync import IB, Option, Stock
-from ib_insync.order import MarketOrder, Order
+from ib_insync import IB, Option, Stock, TagValue, util
+from ib_insync.order import LimitOrder, MarketOrder
+
 from qt.qtrade_client import QuestradeClient
-from utils.util import while_n_times
+from utils.util import while_n_times, midpoint_or_market_price
 
 
 class NopeStrategy:
     QT_ACCESS_TOKEN = 'qt/access_token.yml'
+    SYMBOL = "SPY"
 
     def __init__(self, config, ib: IB):
         self.config = config
         self.ib = ib
         self._nope_value = 0
         self._underlying_price = 0
-        self._portfolio = []
         self.qt = QuestradeClient(token_yaml=self.QT_ACCESS_TOKEN)
         self.run_qt_tasks()
 
     def req_market_data(self):
         # https://interactivebrokers.github.io/tws-api/market_data_type.html
-        self.ib.reqMarketDataType(4)
+        self.ib.reqMarketDataType(1)
+        self.ib.reqAllOpenOrders()
+        self.ib.reqPositions()
 
     def set_nope_value(self):
         self._nope_value, self._underlying_price = self.qt.get_nope()
 
-    def update_portfolio(self):
-        self._portfolio = self.ib.portfolio()
+    def get_portfolio(self):
+        portfolio = self.ib.portfolio()
+        # Filter out non-SPY contracts
+        portfolio = [item for item in portfolio
+                     if item.contract.symbol == self.SYMBOL]
+        return portfolio
+
+    def get_trades(self):
+        trades = self.ib.openTrades()
+        trades = [t for t in trades
+                  if t.isActive() and t.contract.symbol == self.SYMBOL]
+        return trades
 
     # From thetagang
     # https://github.com/brndnmtthws/thetagang
@@ -39,6 +52,7 @@ class NopeStrategy:
                 "Filled",
                 "ApiCancelled",
                 "Cancelled",
+                "PreSubmitted"
             ],
             lambda: self.ib.waitOnUpdate(timeout=5),
             25
@@ -85,11 +99,55 @@ class NopeStrategy:
                 self.ib.placeOrder(contracts[0], order)
             )
 
+    def get_held_contracts(self, portfolio, right):
+        return [c for c in map(lambda p: {'contract': p.contract, 'position': p.position}, portfolio)
+                if c['contract'].right == right
+                and c['position'] > 0]
+
     def exit_positions(self):
-        # TODO: Implement this
-        # If _nope_value > config["nope"]["long_exit"] or _nope_value < config["nope"]["short_exit"]
-        #     Place sell for call/put respectively
-        pass
+        # TODO: Add logging
+        portfolio = self.get_portfolio()
+        trades = self.get_trades()
+        if self._nope_value > self.config["nope"]["long_exit"]:
+            held_calls = self.get_held_contracts(portfolio, 'C')
+            existing_call_order_ids = set(map(lambda t: t.contract.conId,
+                                              filter(lambda t: t.contract.right == 'C' and t.order.action == "SELL", trades)))
+            remaining_calls = list(filter(lambda c: c['contract'].conId not in existing_call_order_ids, held_calls))
+            remaining_calls.sort(key=lambda c: c['contract'].conId)
+
+            if len(remaining_calls) > 0:
+                remaining_call_contracts = [c['contract'] for c in remaining_calls]
+                qualified_contracts = self.ib.qualifyContracts(*remaining_call_contracts)
+                tickers = self.ib.reqTickers(*qualified_contracts)
+                tickers.sort(key=lambda t: t.contract.conId)
+                for idx, ticker in enumerate(tickers):
+                    price = midpoint_or_market_price(ticker)
+                    if not util.isNan(price):
+                        order = LimitOrder("SELL", remaining_calls[idx]['position'], price,
+                                           algoStrategy="Adaptive",
+                                           algoParams=[TagValue(tag='adaptivePriority', value='Normal')],
+                                           tif="DAY")
+                        self.wait_for_trade_submitted(self.ib.placeOrder(ticker.contract, order))
+        elif self._nope_value < self.config["nope"]["short_exit"]:
+            held_puts = self.get_held_contracts(portfolio, 'P')
+            existing_put_order_ids = set(map(lambda t: t.contract.conId,
+                                             filter(lambda t: t.contract.right == 'P' and t.order.action == "SELL", trades)))
+            remaining_puts = list(filter(lambda c: c['contract'].conId not in existing_put_order_ids, held_puts))
+            remaining_puts.sort(key=lambda c: c['contract'].conId)
+
+            if len(remaining_puts) > 0:
+                remaining_put_contracts = [c['contract'] for c in remaining_puts]
+                qualified_contracts = self.ib.qualifyContracts(*remaining_put_contracts)
+                tickers = self.ib.reqTickers(*qualified_contracts)
+                tickers.sort(key=lambda t: t.contract.conId)
+                for idx, ticker in enumerate(tickers):
+                    price = midpoint_or_market_price(ticker)
+                    if not util.isNan(price):
+                        order = LimitOrder("SELL", remaining_puts[idx]['position'], price,
+                                           algoStrategy="Adaptive",
+                                           algoParams=[TagValue(tag='adaptivePriority', value='Normal')],
+                                           tif="DAY")
+                        self.wait_for_trade_submitted(self.ib.placeOrder(ticker.contract, order))
 
     def run_ib(self):
         async def ib_periodic():
@@ -131,5 +189,4 @@ class NopeStrategy:
 
     def execute(self):
         self.req_market_data()
-        self.update_portfolio()
         self.run_ib()

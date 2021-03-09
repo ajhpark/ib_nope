553ba67fe93a5715c0b43d0b6e73ab5cc12267e4
norman-c <52259325+norman-c@users.noreply.github.com>
2021-03-09 12:38:18 -0800

    implemented enter_position

--- example.py
+++ example.py
@@ -10,21 +10,34 @@ ib.reqMarketDataType(4)
 symbol = "SPY"
 stock = Stock(symbol, "SMART", currency="USD")
 contracts = ib.qualifyContracts(stock)
+# print(contracts)
 [ticker] = ib.reqTickers(stock)
 tickerValue = ticker.marketPrice()
-print(tickerValue)
+# print(tickerValue)
 chains = ib.reqSecDefOptParams(stock.symbol, "", stock.secType, stock.conId)
 chain = next(c for c in chains if c.exchange == "SMART")
-print(chain)
+# print(chain)
 
 # get call options for all expirations and strikes within range
+# strikes = [strike for strike in chain.strikes
+#            if strike % 5 == 0
+#            and tickerValue - 20 < strike < tickerValue + 20]
+# contracts = [Option(symbol, expiration, strike, "C", "SMART", tradingClass=chain.tradingClass)
+#              for expiration in chain.expirations
+#              for strike in strikes]
 strikes = [strike for strike in chain.strikes
-           if strike % 5 == 0
-           and tickerValue - 20 < strike < tickerValue + 20]
-contracts = [Option(symbol, expiration, strike, "C", "SMART", tradingClass=chain.tradingClass)
-             for expiration in chain.expirations
-             for strike in strikes]
+        if strike % 5 == 0
+        and tickerValue - 2 < strike < tickerValue + 2]
+expirations = sorted(exp for exp in chain.expirations)[:3]
+rights = ['P', 'C']
+
+contracts = [Option('SPY', expiration, strike, right, 'SMART', tradingClass='SPY')
+        for right in rights
+        for expiration in expirations
+        for strike in strikes]
 
 contracts = ib.qualifyContracts(*contracts)
-tickers = ib.reqTickers(*contracts)
-print(tickers[0])
+# tickers = ib.reqTickers(*contracts)
+print(len(contracts))
+print(contracts[0])
+print(contracts[1])
--- nope/nope_strategy.py
+++ nope/nope_strategy.py
@@ -2,7 +2,9 @@ import asyncio
 from datetime import datetime
 
 from ib_insync import IB, Option, Stock
+from ib_insync.order import MarketOrder, Order
 from qt.qtrade_client import QuestradeClient
+
 
 class NopeStrategy:
     QT_ACCESS_TOKEN = 'qt/access_token.yml'
@@ -22,15 +24,67 @@ class NopeStrategy:
 
     def set_nope_value(self):
         self._nope_value, self._underlying_price = self.qt.get_nope()
+        # self._nope_value, self._underlying_price = -70, 385.0
 
     def update_portfolio(self):
         self._portfolio = self.ib.portfolio()
 
-    def enter_positions(self):
-        # TODO: Implement this
+    # From thetagang
+    def wait_for_trade_submitted(self, trade):
+        while_n_times(
+            lambda: trade.orderStatus.status
+            not in [
+                "Submitted",
+                "Filled",
+                "ApiCancelled",
+                "Cancelled",
+            ],
+            lambda: self.ib.waitOnUpdate(timeout=5),
+            25
+        )
+        return trade
+
+    def find_eligible_contracts(self, symbol, right, min_strike=0, excluded_expirations=[]):
+        stock = Stock(symbol, "SMART", currency="USD")
+        contracts = self.ib.qualifyContracts(stock)
+
+        [ticker] = self.ib.reqTickers(stock)
+        tickerValue = ticker.marketPrice()
+        chains = self.ib.reqSecDefOptParams(
+            stock.symbol, "", stock.secType, stock.conId)
+        chain = next(c for c in chains if c.exchange == "SMART")
+        strikes = [strike for strike in chain.strikes
+                   if strike % 5 == 0
+                   and tickerValue - 2 < strike < tickerValue + 2]
+
+        expirations = sorted(exp for exp in chain.expirations)[:3]
+
+        contracts = [Option('SPY', expiration, strike, right, 'SMART', tradingClass='SPY')
+                     for expiration in expirations
+                     for strike in strikes]
+
+        contracts = self.ib.qualifyContracts(*contracts)
+        return contracts[0]
+
+    def enter_positions(self, quantity=1):
+
+        # TODO: Check portfolio and use adaptive order, add print to file on successful order
         # If _nope_value < config["nope"]["long_enter"] or _nope_value > config["nope"]["short_enter"]
         #     Place buy order for call/put respectively
-        pass
+        if self._nope_value < self.config["nope"]["long_enter"] or self._nope_value > self.config["nope"]["short_enter"]:
+            contract = self.find_eligible_contracts("SPY", "P")
+            order = MarketOrder(
+                "BUY",
+                quantity,
+                # algoStrategy="Adaptive",
+                # algoParams=[TagValue("adaptivePriority", "Patient")],
+                # tif="DAY"
+            )
+
+            # Submit order
+            trade = self.wait_for_trade_submitted(
+                self.ib.placeOrder(contract, order)
+            )
 
     def exit_positions(self):
         # TODO: Implement this
@@ -54,7 +108,6 @@ class NopeStrategy:
     def run_qt_tasks(self):
         # TODO: Implement this
         # After each update, must open/close positions (if necessary) according to the new nope value
-
         async def nope_periodic():
             async def fetch_and_report():
                 self.set_nope_value()
@@ -62,7 +115,10 @@ class NopeStrategy:
                 curr_date = now.strftime("%Y-%m-%d")
                 curr_dt = now.strftime("%Y-%m-%d at %H:%M:%S")
                 with open(f"logs/{curr_date}.txt", "a") as f:
-                    f.write(f'NOPE @ {self._nope_value} | Stock Price @ {self._underlying_price} | {curr_dt}\n')
+                    f.write(
+                        f'NOPE @ {self._nope_value} | Stock Price @ {self._underlying_price} | {curr_dt}\n')
+                    # print(
+                    #     'NOPE @ ', self._nope_value, ' | Stock Price @ ', self._underlying_price, ' | ', curr_dt, '\n')
             while True:
                 await asyncio.gather(asyncio.sleep(60), fetch_and_report())
 
@@ -83,3 +139,11 @@ class NopeStrategy:
         self.run_ib()
 
 
+# From thetagang
+def while_n_times(pred, body, remaining):
+    if remaining <= 0:
+        raise RuntimeError(
+            "Exhausted retries waiting on predicate. This shouldn't happen.")
+    if pred() and remaining > 0:
+        body()
+        while_n_times(pred, body, remaining - 1)

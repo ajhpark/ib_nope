e8a588837348d666a5aaad45f7d1c40e442a7293
Andrew Park <andrewparkdev@gmail.com>
2021-03-09 20:46:04 -0800

    Implement exit_positions (#10)

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,5 @@
+from ib_insync import util
+
 
 # From thetagang
 # https://github.com/brndnmtthws/thetagang
@@ -8,3 +10,12 @@ def while_n_times(pred, body, remaining):
     if pred() and remaining > 0:
         body()
         while_n_times(pred, body, remaining - 1)
+
+
+# From thetagang
+# https://github.com/brndnmtthws/thetagang
+def midpoint_or_market_price(ticker):
+    if util.isNan(ticker.midpoint()):
+        return ticker.marketPrice()
+
+    return ticker.midpoint()
